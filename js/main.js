4f84c7b08e083dce8a5a0733d4f68af5cbd9fa0a
Bo Visker <visk3rtjes@gmail.com>
2023-12-31 10:15:29 +0100

    alles toegevoegd

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// als niet ingelogd = geen profiel popetje en mand, inloggen en registreren 1 grid blokjes naar rechts
+
+// als wel ingelogd = inloggen/registreren en mand 2 grid blokjes naar rechts
